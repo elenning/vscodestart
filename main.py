d9f92d839a5c578b4d2d1408682d8caa54555a57
elenning <erik.lenning@telia.com>
2017-10-18 23:04:28 +0200

    started on main

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
+import CrystalControl
+import CrystalModel
+import CrystalViewer
+
 if __name__ == "__main__":
     print("inside main function")
+
+    controller = CrystalControl()
+    viewer = CrystalViewer()
+    model = CrystalModel()
+
+    controller.start()
+
+    while controller.notDone() == True:
+        controller.update()
