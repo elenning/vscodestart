f97ba4429fa76835fa0c413eebd0a772d50008a1
elenning <erik.lenning@telia.com>
2017-10-18 23:30:07 +0200

    började förstå main

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
+import test
+
 import CrystalControl
 import CrystalModel
 import CrystalViewer
 
+print("Hej, utanförish main")
+
 if __name__ == "__main__":
+    main()
+
+def main():
     print("inside main function")
 
     controller = CrystalControl()
     viewer = CrystalViewer()
     model = CrystalModel()
-
+    
     controller.start()
-
+    
     while controller.notDone() == True:
+    
         controller.update()
+
